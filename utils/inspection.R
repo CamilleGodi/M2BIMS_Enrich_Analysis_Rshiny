eda7f5b990d0814a90d5409611086a79ebf527e4
Camille <camille.godi@gmail.com>
2024-01-24 10:23:21 +0100

    Added docstrings documentation to functions in module "inspection.R"

--- utils/inspection.R
+++ utils/inspection.R
@@ -2,12 +2,26 @@
 
 ### To create Ensembl URL and clickable link (HTML) on the preview table
 
-# Checks the gene ID format corresponds to Ensembl
+#' @description
+#' Checks if a given ID is in Ensemble ID format
+#' @param gene_id : ID to check (string)
+#' @return TRUE if gene_id is an Ensembl ID, FALSE otherwise.
+#'
+#' @example is_Ensembl_ID("ENSGXXXXXXXXXX")
+#' 
 is_Ensembl_ID <- function( gene_id ) {
   status <- ifelse ( str_detect( gene_id, "^ENS[:upper:]+[:digit:]{11}$"), TRUE, FALSE )
   return(status)
 }
 
+#' @description
+#' Create an Ensembl link to website
+#' @param organism : (string) target organism
+#' @param gene_id  : (string) given Ensembl ID
+#' @return (string) Raw link to the Ensembl page corresponding to the given gene_id in the given organism 
+#'
+#' @example create_Ensembl_link("Mus_musculus", "ENSGXXXXXXXXXX")
+#' 
 create_Ensembl_link <- function(organism, gene_id) {
   organism <- sub(" ", "_", organism)
   links <- ifelse( is_Ensembl_ID( gene_id ),
@@ -16,6 +30,14 @@ create_Ensembl_link <- function(organism, gene_id) {
   return(links)
 }
 
+#' @description
+#' Create an Ensembl link to website as a HTML hyperlink, to include in preview data tables
+#' @param organism : (string) target organism
+#' @param gene_id  : (string) given Ensembl ID
+#' @return (string) HTML formatted hyperlink to the Ensembl page corresponding to the given gene_id in the given organism
+#'
+#' @example create_Ensembl_html_link("Mus_musculus", "ENSGXXXXXXXXXX")
+#' 
 create_Ensembl_html_link <- function(organism, gene_id) {
   organism <- sub(" ", "_", organism)
   links <- ifelse( is_Ensembl_ID( gene_id ),
@@ -28,7 +50,15 @@ create_Ensembl_html_link <- function(organism, gene_id) {
 
 ### FILTER DATATABLE
 
-
+#' @description
+#' Filters data table according to given adjusted p-value and Fold Change cut-offs
+#' @param deseq_data  : (data table) differential expression data table
+#' @param fc_cutoff   : (float) Fold Change cut-off (default : 1)
+#' @param padj_cutoff : (float) adjusted p-value cut-off (default : 0.05)
+#' @return (data table) differential expression data table without non-differential-expressed genes. new columns : "log2FC" and "diff_expressed" ("UP", "DOWN")
+#'
+#' @example filter_dt(deseq_datatable, 2, 0.01)
+#' 
 filter_dt <- function(deseq_data, fc_cutoff = 1, padj_cutoff = 0.05) {
   deseq_data$diff_expressed <- ifelse(
     deseq_data$log2FC <= - fc_cutoff & deseq_data$padj <= padj_cutoff, "DOWN",
@@ -42,9 +72,23 @@ filter_dt <- function(deseq_data, fc_cutoff = 1, padj_cutoff = 0.05) {
 
 
 ### VOLCANO PLOT
-
+#' @description
+#' Draws a ggplot2 based volcano plot of given differential expression data, pr
+#' @param deseq_data  : (data table) differential expression data table
+#' @param title       : (string) volcano plot title (defauklt : "Volcano plot")
+#' @param xlab        : (string) label of the x axis (default : "Log2(FoldChange)")
+#' @param ylab        : (string) label of the x axis (default : "-Log10(p-value adjusted)")
+#' @param xlim        : (vector c(float,float) ) limits of the x axis (default : NA)
+#' @param ylim        : (vector c(float,float) ) limits of the y axis (default : NA)
+#' @param fc_cutoff   : (float) Fold Change cut-off (default : 1)
+#' @param padj_cutoff : (float) adjusted p-value cut-off (default : 0.05)
+#' @param lines       : (bool) whether lines should be drawn to represent the FC and adjuste p-value cut_offs
+#' @return (ggplot2 plot) volcano plot of given differentially expression data table
+#'
+#' @example draw_volcano(deseq_datatable, fc_cutoff = 2, padj_cutoff = 0.01, lines = TRUE)
+#' 
 draw_volcano <- function(deseq_data,
-                        title = "Volcanoplot",
+                        title = "Volcano plot",
                         xlab = "Log2(FoldChange)",
                         ylab = "-Log10(p-value adjusted)",
                         xlim = NA,
@@ -52,6 +96,7 @@ draw_volcano <- function(deseq_data,
                         fc_cutoff = 1,
                         padj_cutoff = 0.05,
                         lines = FALSE) {
+  
   deseq_data <- deseq_data[!is.na(deseq_data$padj), ]       # retirer les valeurs n'ayant pas passé le filtre indépendant si resultat de DESeq2
   deseq_data$diff_expressed <- ifelse(
     deseq_data$log2FC <= -fc_cutoff & deseq_data$padj <= padj_cutoff, "DOWN",
