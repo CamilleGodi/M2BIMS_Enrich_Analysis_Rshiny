e54dceae8c344de44916a67b0f5760dcc1371958
Camille Godi <camille.godi@gmail.com>
2024-03-27 13:42:42 +0100

    enhanced ORA module

--- utils/ORA.R
+++ utils/ORA.R
@@ -18,7 +18,6 @@ source("./utils/enrichment.R")
 #' @example do_ora_go_terms(filtered_data,"org.Hs.eg.db", "MF", 0.01, 0.05, 0.05)
 #'
 do_ora_go_terms <- function(reactive_annotated_data, organism_db, ontology, p_value_cutoff, p_adj_cutoff, q_value_cutoff) {
-  table_filtered_new_ids <- prepare_pipe(reactive_annotated_data, organism_db = organism_db, "ENSEMBL")
   
   ora_ids <- prepare_ora(table_filtered_new_ids)
   universe <- prepare_universe(reactive_annotated_data)
@@ -37,18 +36,20 @@ do_ora_go_terms <- function(reactive_annotated_data, organism_db, ontology, p_va
 
 
 # TMP : Tableau d'entré (non filtré etc), réactive "filtered_data"
-table_filtered_tmp <-
-  read.csv("./Example_files/filtered_data.csv", sep = ",")
+table_filtered_tmp <- read.csv("./Example_files/filtered_data.csv", sep = ",")
 
 # TMP : ext variables -> replace with the reactive variables
 organism_library_go_tmp <- organism_conversion_table["Mus musculus", "annotation_db"]
 p_value_cutoff_tmp <- 0.01
 p_adj_cutoff_tmp   <- 0.05
 q_value_cutoff_tmp <- 0.05
-ontology_tmp <- c("MF", "CC")
+ontology_tmp <- "MF"
+
+
+table_filtered_new_ids <- prepare_pipe(table_filtered_tmp, organism_db = organism_library_go_tmp, "ENSEMBL")
 
 res_tmp <- do_ora_go_terms(
-    table_filtered_tmp,
+    table_filtered_new_ids,
     organism_library_go_tmp,
     ontology_tmp,
     p_value_cutoff_tmp,
@@ -59,25 +60,33 @@ res_tmp <- do_ora_go_terms(
 res_tmp %>% enrich_pagination(alpha_cutoff = 0.05)
 
 res_tmp %>% draw_dotplot(show_category = 30,
-                         title = "ORA - GO termes - Dot plot")
+                         title = paste("ORA - GO termes -",
+                                       ontology_tmp,
+                                       "- Dot plot"))
 
 res_tmp %>% draw_cnetplot(
   category_label = 0.6,
   gene_list = res_tmp@result$geneID,
   category_color = "red",
   node_label = "category",
-  title = "ORA - GO termes - CNET plot"
+  title = paste("ORA - GO termes -",
+                ontology_tmp,
+                "- CNET plot")
 )
 
 res_tmp %>% draw_treeplot(
-  gradient_col = c("green", "black"),
+  gradient_col = c("red", "blue"),
   show_category = 30,
   n_cluster = 10,
   label_words_n = 4,
   h_clust_method = "ward.D2",
-  title = "ORA - GO termes - Tree plot"
+  title = paste("ORA - GO termes -",
+                ontology_tmp,
+                "- Tree plot")
 )
 
 res_tmp %>% draw_emapplot(show_category = 10,
                           category_label = 0.5,
-                          title = "ORA - GO termes - EMAP plot")
+                          title = paste("ORA - GO termes -",
+                                        ontology_tmp,
+                                        "- EMAP plot"))
