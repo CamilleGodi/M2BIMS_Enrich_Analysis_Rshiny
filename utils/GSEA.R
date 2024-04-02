2aa80843afce91b9f1be5356508e7ca4e1f709c5
Victor Bailleul <victor.bailleul76240gmail.com>
2024-04-02 16:18:17 +0200

    Modification gsea.R

--- utils/GSEA.R
+++ utils/GSEA.R
@@ -14,15 +14,23 @@
 #'
 #' @example do_gsea_go_terms(filtered_data,"org.Hs.eg.db", "MF", 0.01, 0.05, 0.05)
 #'
-do_gsea_go_terms <- function(reactive_annotated_data, organism_db, universe, ontology, p_value_cutoff, p_adj_cutoff, q_value_cutoff) {
+do_gsea_go_terms <- function(reactive_annotated_data, organism_db, universe, ontology, p_value_cutoff, p_adj_cutoff, q_value_cutoff, min_GS_size, max_GS_size) {
   
-  gsea_ids <- prepare_gsea(reactive_annotated_data)
+  gsea_ids <- prepare_gsea(reactive_annotated_data,
+                           metric = "log2FC",
+                          abs = TRUE)
   
-  gsea_go <- load_gsea_go(gsea_ids, universe, organism_db)
+  gsea_go <- load_gsea_GO_enrichment(gsea_ids, 
+                                     universe = universe, 
+                                     organism_db = organism_db, 
+                                     min_GS_size = 10, 
+                                     max_GS_size = 50)
+  
   gsea_go_after_filter <- filter_table_enrich_results(gsea_go, 
                                                      p_value_cutoff = p_value_cutoff, 
                                                      p_adj_cutoff   = p_adj_cutoff, 
                                                      q_value_cutoff = q_value_cutoff)
+  
   gsea_go_after_filter_ontologies <- filter_go_enrich_results(gsea_go_after_filter, ontology = ontology)
   
   return(gsea_go_after_filter_ontologies)
@@ -45,11 +53,13 @@ do_gsea_go_terms <- function(reactive_annotated_data, organism_db, universe, ont
 #'
 do_gsea_kegg <- function(reactive_annotated_data, organism_db, universe, kegg_organism_code, p_value_cutoff, p_adj_cutoff, q_value_cutoff) {
   
-  gsea_ids <- prepare_gsea(reactive_annotated_data)
+  gsea_ids <- prepare_gsea(reactive_annotated_data,
+                           metric = "log2FC",
+                           abs = TRUE)
   
-  gsea_kegg <- load_gsea_kegg(gene_list = gsea_ids,
-                            organism_db = kegg_organism_code,
-                            universe = universe)
+  gsea_kegg <- load_gsea_kegg_enrichment(gene_list = gsea_ids,
+                                         organism_db = kegg_organism_code,
+                                         universe = universe)
   
   gsea_kegg_after_filter <- filter_table_enrich_results(gsea_kegg, 
                                                        p_value_cutoff = p_value_cutoff, 
